5078f925f09b2936633d32ff75ce7b1176e922f2
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-12 21:10:00 +0530

    6

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require ("express");
+const express = require("express");
+const cors = require("cors");
 const quizzes = require("./db/quizzes");
 
 const app = express();
-
 const PORT = 3000;
 
-// app.get('/',(req,res) => {
-//     res.send("hello geeks");
-// })
+app.use(cors()); // Enable CORS for all routes
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     console.log(quizzes);
     res.send("quizzes");
-})
+});
 
 app.listen(process.env.PORT || PORT, () => {
     console.log("server started");
-})
+});
