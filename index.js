25267ac3063e78533864e963443d3344b8a2abc2
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-12 15:53:04 +0530

    added db

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require ("express");
-const quizzes = require("./db/quizzes")
+const quizzes = require("./db/quizzes");
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/',(req,res) => {
-    res.send("hello geeks");
-})
+// app.get('/',(req,res) => {
+//     res.send("hello geeks");
+// })
 
 app.get('/',(req,res) => {
-    res.send("quizzes")
+    res.send("quizzes");
 })
 
 app.listen(process.env.PORT || PORT, () => {
