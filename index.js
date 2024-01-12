39182350108a40bc8a51f1d930e2337688ba507c
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-12 21:14:01 +0530

    7

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const quizzes = require("./db/quizzes");
 const app = express();
 const PORT = 3000;
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
 app.get('/', (req, res) => {
+    console.log("Received a request"); // Add more logging statements
     console.log(quizzes);
+
+    // Simulate an error for testing
+    // Uncomment the next line to see if it affects the response
+    // throw new Error("Intentional error");
+
     res.send("quizzes");
 });
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log("server started");
+    console.log("Server started");
 });
