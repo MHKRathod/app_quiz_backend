dfd478768527841f0fa854e8129e1a1a86c98d28
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-23 18:02:15 +0530

    login feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const quizRouter = require('./router/quiz.router')
+const quizRouter = require('./router/quiz.router');
+const userdata = require("./db/users");
 const cors = require ('cors');
 const app = express();
 
@@ -15,6 +16,17 @@ app.get('/', (req, res) => {
 });
 
 app.use('/quiz',quizRouter);
+
+app.post("/auth/login",(req,res) => {
+    const {username,password} = req.body;
+    const isUserNameVerified = userdata.users.some(user => user.username === username && user.password === password)
+    if(isUserNameVerified){
+        res.json("userverified")
+    } 
+    else{
+        res.status(401).json({message:"invalidcredentials"})
+    }
+})
 
 app.listen(process.env.PORT || PORT, () => {
     console.log("Server started");
