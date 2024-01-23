6c86f91a07b49f9305bb3670bbc67a4e12573b74
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-23 09:48:01 +0530

    modified index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const quizzes = require("./db/quizzes");
+
 
 const app = express();
 const PORT = 3000;
@@ -8,14 +8,11 @@ const PORT = 3000;
 app.use(cors());
 
 app.get('/', (req, res) => {
-    console.log("Received a request"); // Add more logging statements
-    console.log(quizzes);
+    res.send("hello geeks");
 
     // Simulate an error for testing
     // Uncomment the next line to see if it affects the response
     // throw new Error("Intentional error");
-
-    res.send("quizzes");
 });
 
 app.listen(process.env.PORT || PORT, () => {
