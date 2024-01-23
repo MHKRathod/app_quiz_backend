097bfa694662409aad54c9c6a2bf411d69adb5d3
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-23 10:02:34 +0530

    first

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const cors = require("cors");
-
-
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("hello geeks");
 
-    // Simulate an error for testing
-    // Uncomment the next line to see if it affects the response
-    // throw new Error("Intentional error");
 });
 
 app.listen(process.env.PORT || PORT, () => {
