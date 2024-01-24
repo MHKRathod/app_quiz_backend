3aa7f746be5404abe08ade7e6b32a87ce5be5f2c
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-24 19:42:27 +0530

    79

--- router/quiz.router.js
+++ router/quiz.router.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const quizRouter = express.Router();
 const quizzes = require('../db/quizzes');
 
 
 quizRouter.route("/")
          .get((req,res) => {
-            res.send(quizzes.data)
+            res.json(quizzes.data)
     });
 
 module.exports = quizRouter;    
