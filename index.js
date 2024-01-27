ab0b6208c51efbf3cd91ff98c420a7ea53b309d5
HARI KRISHNA <harikrishnarathod52@gmail.com>
2024-01-27 15:18:44 +0530

    added middle ware request not found

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const quizRouter = require('./router/quiz.router');
-const {loginRouter,signupRouter,authVerify } = require("./router/auth.router");
 
+const {loginRouter,signupRouter} = require("./router/auth.router");
+const routeNotFound = require("./middleware/routeNotFound")
 const cors = require ('cors');
 const app = express();
 
@@ -21,6 +22,7 @@ app.use('/quiz',quizRouter);
 
 app.use('/auth/login',loginRouter)
 app.use('/auth/signup',signupRouter)
+app.use(routeNotFound);
 
 app.listen(process.env.PORT || PORT, () => {
     console.log("Server started");
